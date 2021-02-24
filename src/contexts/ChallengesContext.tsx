e01dea571de650f719aee9c2eeb41d8742583e63
hereisjohnny2 <joaomarceloccarvalho@gmail.com>
2021-02-24 16:23:47 -0300

    ExperienceBar updates xp info

--- src/contexts/ChallengesContext.tsx
+++ src/contexts/ChallengesContext.tsx
@@ -1,11 +1,22 @@
 import { createContext, ReactNode, useState } from 'react';
 
+import challenges from '../../challenges.json';
+
+interface Challenge {
+  type: 'body' | 'eye';
+  description: string;
+  amount: number,
+}
+
 interface ChallengesContextData {
   level: number,
   currentExperience: number,
   challengesCompleted: number,
+  experienceToNextLevel: number,
+  activeChallenge: Challenge,
   levelUp: () => void,
   startNewChallenge: () => void,
+  resetChallenge: () => void,
 }
 
 interface ChallengesProviderProps {
@@ -18,17 +29,26 @@ export const ChallengesContext = createContext({} as ChallengesContextData);
 export function ChallengesProvider({ children }: ChallengesProviderProps) {
 
   const [level, setLevel] = useState(1);
-  const [currentExperience, setCurrentExperience] = useState(0);
+  const [currentExperience, setCurrentExperience] = useState(60);
   const [challengesCompleted, setChallengesCompleted] = useState(0);
 
+  const [activeChallenge, setActiveChallenge] = useState(null);
 
+  const experienceToNextLevel = Math.pow((level + 1) * 4, 2)
 
   function levelUp() {
     setLevel(level + 1)
   }
 
   function startNewChallenge() {
-    console.log('new challenge!');
+    const activeChallengeIndex = Math.floor(Math.random() * challenges.length);
+    const challenge = challenges[activeChallengeIndex];
+
+    setActiveChallenge(challenge);
+  }
+
+  function resetChallenge() {
+    setActiveChallenge(null);
   }
 
   return (
@@ -37,8 +57,11 @@ export function ChallengesProvider({ children }: ChallengesProviderProps) {
         level,
         currentExperience,
         challengesCompleted,
+        activeChallenge,
+        experienceToNextLevel,
         levelUp,
         startNewChallenge,
+        resetChallenge,
       }}
     >
       {children}
